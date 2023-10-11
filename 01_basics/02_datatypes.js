c553ad57f242de1b31e9ffefe3085d4a3cd536a8
Sameer Choudhary <72244733+choudhary-sameer@users.noreply.github.com>
2023-10-11 18:45:41 +0000

    Datatypes and ECMA standards

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+// code readability should be high
+console.log("Sameer")
+
+let name = "Sameer" // string
+let age = 23 // int
+let isLoggedIn = true // boolean
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => value not initialized
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
