c90cee446595acbae56342daaf63005ad7a36fd6
Sameer Choudhary <72244733+choudhary-sameer@users.noreply.github.com>
2023-10-14 15:32:59 +0000

    Datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Sameer" 
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// "45" => 45
+// "54abc" => NaN 
+// true => 1; false => 0
+
+
+let isLoggedIn = "Sameer"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false; "Sameer" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
