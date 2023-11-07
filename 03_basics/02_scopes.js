f375b1e9f55e22e081d215fdfe60b066f17771f3
Sameer Choudhary <72244733+choudhary-sameer@users.noreply.github.com>
2023-11-07 11:39:15 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,6 +9,50 @@ if (true) {
 
 // scope is different for browser and vs code
 
-console.log(a);
+// console.log(a);
 // console.log(b)
 // console.log(c)
+
+function one(){
+  const username = "hitesh"
+
+  function two(){
+      const website = "youtube"
+      console.log(username);
+  }
+  // console.log(website);
+
+   two()
+
+}
+
+// one()
+
+if (true) {
+  const username = "hitesh"
+  if (username === "hitesh") {
+      const website = " youtube"
+      // console.log(username + website);
+  }
+  // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ INTERESTING ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+  return num + 1
+}
+
+
+addTwo(5) // will give an error
+const addTwo = function(num){ // This is an expression
+  return num + 2
+}
+
+// To Read : https://developer.mozilla.org/en-US/docs/Glossary/Hoisting
