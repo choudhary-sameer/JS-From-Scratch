c2ed92a713708b6ebb48bc68b7fe747680679128
Sameer Choudhary <72244733+choudhary-sameer@users.noreply.github.com>
2023-12-03 14:55:02 +0000

    various loops and map(), filter(), reduce()

---
+++ 05_iterations/reduce.js
@@ -0,0 +1,36 @@
+// The reducer walks through the array element-by-element, at each step adding the current array value to the result from the previous step (this result is the running sum of all the previous steps) — until there are no more elements to add.
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+// const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+// console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
